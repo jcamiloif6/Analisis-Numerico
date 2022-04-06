0999138cd908998dfc63304c32972206043abc9a
Jose Mejia <jmejia@splashfinancial.com>
2022-04-06 10:11:58 -0500

    Adding missing urls

--- analisisnumerico/methods/urls.py
+++ analisisnumerico/methods/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('bisection', views.bisection, name='bisection'),
     path('newton', views.newton, name='newton'),
+    path('puntofijo', views.puntofijo, name='puntofijo'),
+    path('busquedasincrementales', views.busquedasincrementales, name='busquedasincrementales'),
+    path('reglafalsa', views.reglafalsa, name='reglafalsa'),
+    path('secante', views.secante, name='secante'),
+    path('raicesmultiples', views.raicesmultiples, name='raicesmultiples'),
 ]
