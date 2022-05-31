5a9e3007f4f1db7600385fcba100f36cfed873e1
Jose Mejia <jmejia@splashfinancial.com>
2022-05-30 19:46:52 -0500

    fix on bisection method

--- analisisnumerico/methods/utils/biseccion.py
+++ analisisnumerico/methods/utils/biseccion.py
@@ -1,9 +1,7 @@
 import numpy as np
-from sympy import symbols
-from sympy import lambdify
-from sympy import sympify
+from sympy import symbols, lambdify, sympify
 
-def calcular (a, b, crit, fn):
+def calcular (a, b, crit, fn, nMax):
     answer = {
         "response": "La función no tiene una raíz en el intervalo de " + "x = " + str(a) + " a x = " + str(b),
         "pasos": []}
@@ -14,19 +12,16 @@ def calcular (a, b, crit, fn):
     ea = 1
     x_anterior = 0
 
-    if f(a) * f(b) < 0:
-        while ea > crit:
-            xr = (a + b)/2
-            ea = abs(xr - x_anterior)
-            if f(xr) * f(a) < 0:
-                b = xr
-            else:
-                a = xr
-            x_anterior = xr
-            answer["pasos"].append("{:^10} {:^10} {:^10} {:^10} {:^10}".format(i, a, b, xr, ea))
-            i += 1
-        answer["response"] = "El valor de x es " + round(xr, 9) + " con un error de " + ea
-    else:
-        answer["pasos"].append("{:^10} {:^10} {:^10} {:^10} {:^10}".format("i", "a", "b", "xr", "ea"))
+    while ea > crit and i < nMax:
+        xr = (a + b)/2
+        ea = abs(xr - x_anterior)
+        if f(xr) * f(a) < 0:
+            b = xr
+        else:
+            a = xr
+        x_anterior = xr
+        answer["pasos"].append("{:^10} {:^10} {:^10} {:^10} {:^10}".format(i, a, b, xr, ea))
+        i += 1
+    answer["response"] = "El valor de x es " + str(round(xr, 9)) + " con un error de " + str(ea)
     
     return answer
--- analisisnumerico/methods/views.py
+++ analisisnumerico/methods/views.py
@@ -1,5 +1,7 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from methods.utils import biseccion, newton, puntofijo, busquedasincrementales, reglafalsa, secante, raicesmultiples, eliminaciongaussianasimple, factorizacionlu, crout, doolittle, gaussseidel, jacobi
 
@@ -7,137 +9,165 @@ from methods.utils import biseccion, newton, puntofijo, busquedasincrementales, 
 def index(request):
     return HttpResponse("Hello, world. App's backend is up and running.")
 
+@csrf_exempt
 def bisection(request):
-    a = float(request.GET['a'])
-    b = float(request.GET['b'])
-    crit = float(request.GET['crit'])
-    fn = request.GET['fn']
-
-    response = biseccion.calcular(a, b, crit, fn)
+    body = json.loads(request.body)
+    a = float(body['a'])
+    b = float(body['b'])
+    crit = float(body['crit'])
+    fn = str(body['fn'])
+    nMax = int(body['nMax'])
+    response = biseccion.calcular(a, b, crit, fn, nMax)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def newton(request):
-    fn = request.GET['fn']
-    x0 = float(request.GET['x0'])
-    n = float(request.GET['n'])
-    tol = float(request.GET['tol'])
+    body = json.loads(request.body)
+    fn = body['fn']
+    x0 = float(body['x0'])
+    n = float(body['n'])
+    tol = float(body['tol'])
 
     response = newton.calcular(fn, x0, n, tol)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def puntofijo(request):
-    gn = request.GET['gn']
-    x0 = float(request.GET['x0'])
-    n = float(request.GET['n'])
-    tol = float(request.GET['tol'])
+    body = json.loads(request.body)
+    gn = body['gn']
+    x0 = float(body['x0'])
+    n = float(body['n'])
+    tol = float(body['tol'])
 
     response = puntofijo.calcular(gn, x0, n, tol)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def busquedasincrementales(request):
-    fn = request.GET['fn']
-    x0 = float(request.GET['x0'])
-    h = float(request.GET['h'])
-    nMax = float(request.GET['nMax'])
+    body = json.loads(request.body)
+    fn = body['fn']
+    x0 = float(body['x0'])
+    h = float(body['h'])
+    nMax = float(body['nMax'])
 
     response = busquedasincrementales.calcular(gn, x0, h, nMax)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def reglafalsa(request):
-    fn = request.GET['fn']
-    a = float(request.GET['a'])
-    b = float(request.GET['b'])
-    tol = float(request.GET['tol'])
-    nMax = float(request.GET['nMax'])
+    body = json.loads(request.body)
+    fn = body['fn']
+    a = float(body['a'])
+    b = float(body['b'])
+    tol = float(body['tol'])
+    nMax = float(body['nMax'])
 
     response = reglafalsa.calcular(fn, a, b, tol, nMax)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def secante(request):
-    fn = request.GET['fn']
-    x0 = float(request.GET['x0'])
-    x1 = float(request.GET['x1'])
-    tol = float(request.GET['tol'])
-    nMax = float(request.GET['nMax'])
+    body = json.loads(request.body)
+    fn = body['fn']
+    x0 = float(body['x0'])
+    x1 = float(body['x1'])
+    tol = float(body['tol'])
+    nMax = float(body['nMax'])
 
     response = secante.calcular(fn, x0, x1, tol, nMax)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def raicesmultiples(request):
-    fn = request.GET['fn']
-    df = request.GET['df']
-    d2f = request.GET['d2f']
-    x0 = float(request.GET['x0'])
-    tol = float(request.GET['tol'])
-    nMax = float(request.GET['nMax'])
+    body = json.loads(request.body)
+    fn = body['fn']
+    df = body['df']
+    d2f = body['d2f']
+    x0 = float(body['x0'])
+    tol = float(body['tol'])
+    nMax = float(body['nMax'])
 
     response = raicesmultiples.calcular(fn, df, d2f, x0, tol, nMax)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def gauss_simple(request):
-    A = request.GET['A']
-    b = request.GET['b']
+    body = json.loads(request.body)
+    A = body['A']
+    b = body['b']
 
     response = eliminaciongaussianasimple.calcular(A, b)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def fac_lu_simple(request):
-    A = request.GET['A']
-    b = request.GET['b']
+    body = json.loads(request.body)
+    A = body['A']
+    b = body['b']
 
     response = factorizacionlu.calcular(A, b)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def crout(request):
-    A = request.GET['A']
-    b = request.GET['b']
+    body = json.loads(request.body)
+    A = body['A']
+    b = body['b']
 
     response = crout.calcular(A, b)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def doolittle(request):
-    A = request.GET['A']
-    b = request.GET['b']
+    body = json.loads(request.body)
+    A = body['A']
+    b = body['b']
 
     response = doolittle.calcular(A, b)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def cholesky(request):
-    A = request.GET['A']
-    b = request.GET['b']
+    body = json.loads(request.body)
+    A = body['A']
+    b = body['b']
 
     response = cholesky.calcular(A, b)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def gauss_seidel(request):
-    A = request.GET['A']
-    b = request.GET['b']
-    x = request.GET['x']
-    tol = request.GET['tol']
-    nMax = request.GET['nMax']
+    body = json.loads(request.body)
+    A = body['A']
+    b = body['b']
+    x = body['x']
+    tol = body['tol']
+    nMax = body['nMax']
 
     response = gaussseidel.calcular(A, b, x, tol, nMax)
 
     return JsonResponse(response)
 
+@csrf_exempt
 def jacobi(request):
-    A = request.GET['A']
-    b = request.GET['b']
-    x = request.GET['x']
-    tol = request.GET['tol']
-    nMax = request.GET['nMax']
+    body = json.loads(request.body)
+    A = body['A']
+    b = body['b']
+    x = body['x']
+    tol = body['tol']
+    nMax = body['nMax']
 
     response = jacobi.calcular(A, b, x, tol, nMax)
 
